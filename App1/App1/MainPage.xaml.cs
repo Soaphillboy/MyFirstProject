f9d3e6776f55d3a9584109d261cc34604f1e3949
Jānis Jansons <Jnis Jansons>
2016-08-24 17:38:06 +0300

    first commit

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void SignInClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new StartPage());
+        }
+        
+ 
+    }
+}
---
+++ App1/App1/StartPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+        async void AddPlaceClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddPlacePage());
+        }
+        async void MyPlacesClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MyPlacesPage());
+        }
+    }
+}
